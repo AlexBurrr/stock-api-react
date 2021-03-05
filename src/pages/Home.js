57bd2757058932c7f83d125a860290f22b2c1e10
Alex Berumen <alexandroberumen@gmail.com>
2021-03-04 20:35:43 -0800

    connected to TD api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,31 @@
 import React from 'react'
+import axios from 'axios'
 
 const Home = () => {
+
+    let stock = 'FGEN'
+    let key = 'XFQMEZGTK4PCZO3HKDM1AWE6YDF1GEEB'
+    let periodLength = 'year'
+
+
+    axios.get(`https://api.tdameritrade.com/v1/marketdata/${stock}/pricehistory`, {
+        params: {
+            apikey: key,
+            periodType: periodLength,
+            period: 1,
+            frequencyType: 'daily',
+            frequency: 1,
+            endDate: 1614758400000,
+            startDate: 1614672000000,
+            needExtendedHoursData: false
+        }
+    })
+        .then((response) => console.log(response))
+        ;
+
+
+
+
     return (
         <div>
             hi
